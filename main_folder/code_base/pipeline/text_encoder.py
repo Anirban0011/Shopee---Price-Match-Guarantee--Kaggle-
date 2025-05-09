31dd030f15cbd7690b88f190534a2c1b2874b2bb
Anirban0011 <bhattandbhatt.999@gmail.com>
2025-05-09 13:14:34 +0530

    update

--- main_folder/code_base/pipeline/text_encoder.py
+++ main_folder/code_base/pipeline/text_encoder.py
@@ -13,8 +13,6 @@ class TextEncoder(nn.Module):
         max_seq_length=35,
         backbone=None,
         arcmargin=0.5,
-        alpha=1e-4,
-        use_dynamic_margin=False,
     ):
         super().__init__()
         self.backbone_name = backbone
@@ -22,34 +20,31 @@ class TextEncoder(nn.Module):
         self.out_features = num_classes
         self.embed_size = embed_size
         self.margin = arcmargin
-        self.alpha = alpha
-        self.use_dynamic_margin = use_dynamic_margin
+
         self.arcface = ArcMarginProduct(
-            in_features=self.embed_size,
+            in_features=self.backbone.config.hidden_size,
             out_features=self.out_features,
             m=self.margin,
-            use_dynamic_margin=self.use_dynamic_margin,
-            alpha=self.alpha,
         )
         self.pool = nn.AvgPool1d(kernel_size=max_seq_length)
-        self.bn = nn.BatchNorm1d(self.embed_size)
+        self.bn = nn.BatchNorm1d(self.backbone.config.hidden_size)
 
-    def forward(self, input_ids, attention_mask, labels=None, epoch=0):
-        if any(x in self.backbone_name for x in ["bert-base", "roberta-base"]):
-            features = self.backbone(
-                input_ids, attention_mask=attention_mask, output_hidden_states=True
-            ).hidden_states[-2:]
-            features = torch.cat([features[-1], features[-2]], dim=-1)
-            features = features[:, :, : self.embed_size]
-        else:
-            features = self.backbone(
-                input_ids, attention_mask=attention_mask
-            ).last_hidden_state
+    def forward(self, input_ids, attention_mask, labels=None):
+        # if any(x in self.backbone_name for x in ["bert-base", "roberta-base"]):
+        #     features = self.backbone(
+        #         input_ids, attention_mask=attention_mask, output_hidden_states=True
+        #     ).hidden_states[-2:]
+        #     features = torch.cat([features[-1], features[-2]], dim=-1)
+        #     features = features[:, :, : self.embed_size]
+        # else:
+        features = self.backbone(
+            input_ids, attention_mask=attention_mask
+        ).last_hidden_state
         features = features.transpose(1, 2)
         features = self.pool(features)
         features = features.view(features.size(0), -1)
         features = self.bn(features)
         features = F.normalize(features)
         if labels is not None:
-            return self.arcface(features, labels, epoch)
+            return self.arcface(features, labels)
         return features

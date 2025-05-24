2ac2fd8d53607f4c8c544520c4647bfd2c0b2d7c
Anirban0011 <bhattandbhatt.999@gmail.com>
2025-05-24 18:05:14 +0530

    update

--- main_folder/code_base/utils/__init__.py
+++ main_folder/code_base/utils/__init__.py
@@ -4,5 +4,5 @@ from .f1_score import row_wise_f1_score
 from .train import train_img_model, train_text_model
 from .valid import valid_img_model, valid_text_model
 from .gen_feat import gen_img_feats, gen_text_feats
-from .currface import CurricularFacefrom
+from .currface import CurricularFace
 from .lr_schedule import WarmupScheduler

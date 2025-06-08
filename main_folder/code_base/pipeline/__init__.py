f24e8007f13c177c01b63c1c2a4a7e792e747a79
Anirban0011 <bhattandbhatt.999@gmail.com>
2025-06-08 10:14:56 +0530

    upd

--- main_folder/code_base/pipeline/__init__.py
+++ main_folder/code_base/pipeline/__init__.py
@@ -4,3 +4,4 @@ from .image_encoder import ImgEncoder
 from .text_encoder import TextEncoder
 from .neck import Neck1
 from .gempool import GeM
+from .depthconv import DepthwiseSeparableConv
---
+++ main_folder/code_base/pipeline/depthconv.py
@@ -0,0 +1,13 @@
+import torch
+import torch.nn as nn
+
+class DepthwiseSeparableConv(nn.Module):
+    def __init__(self, in_channels=3072, out_channels=1793):
+        super(DepthwiseSeparableConv, self).__init__()
+        self.depthwise = nn.Conv2d(in_channels, in_channels, kernel_size=3, padding=1, groups=in_channels)
+        self.pointwise = nn.Conv2d(in_channels, out_channels, kernel_size=1)
+
+    def forward(self, x):
+        x = self.depthwise(x)
+        x = self.pointwise(x)
+        return x
